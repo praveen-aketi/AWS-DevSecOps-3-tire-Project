b0c23e38b79efd680c17c2ffc927d53f705675b5
praveen-aketi <prvn.aketi@gmail.com>
2025-08-06 16:27:51 +0530

    added new items

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>í°¾ Welcome to SecurePetStore</h1>
+      <p>Frontend hosted securely on AWS S3 + CloudFront</p>
+    </div>
+  );
+}
+
+export default App;
+
