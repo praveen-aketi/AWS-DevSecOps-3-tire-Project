4eaf0b07e2b689677203004be72a782741c896f7
praveen-aketi <prvn.aketi@gmail.com>
2025-11-21 14:07:03 +0530

    fix: add missing iam role for ecs task execution

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -141,6 +141,29 @@ output "ecr_repo_url" {
   value = aws_ecr_repository.backend.repository_url
 }
 
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "backend" {
   family                   = "backend-task"
   network_mode             = "awsvpc"
